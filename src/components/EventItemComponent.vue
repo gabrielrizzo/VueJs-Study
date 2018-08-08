24cabb1393eb25b0aacd16cd88f72febe759bf4e
Gabriel Rizzo <grizzo01@hotmail.com>
2018-08-08 14:49:21 -0300

    Added share dialog in eventItemComponent

--- src/components/EventItemComponent.vue
+++ src/components/EventItemComponent.vue
@@ -34,7 +34,7 @@
               <md-button class="md-icon-button">
               <md-icon>favorite</md-icon>
             </md-button>
-            <md-button class="md-icon-button">
+            <md-button class="md-icon-button" @click="active = true">
               <md-icon>share</md-icon>
             </md-button>
             <md-button class="md-icon-button" @click="delEvent(index,event.id)">
@@ -45,7 +45,20 @@
       </div>
     </md-card-area>
   </md-card>
-  </div>
+  <md-dialog-confirm
+    :md-active.sync="active"
+    :md-title="'Do you want to share '+event.title+'?'"
+    md-content="Just a test, it will not share in your social media"
+    md-confirm-text="Share"
+    md-cancel-text="Cancel"
+    @md-cancel="onCancel"
+    @md-confirm="onConfirm"/>
+
+    <md-snackbar md-position="center" md-duration="3000" :md-active.sync="showSnackbar" md-persistent>
+      <span>{{message}}</span>
+      <md-button class="md-primary" @click="showSnackbar = false">Dismiss</md-button>
+    </md-snackbar>
+</div>
 
 </template>
 
@@ -54,6 +67,14 @@
 import {mapActions} from 'vuex'
 import store from '../vuex/store'
 export default {
+  data() {
+    return {
+      active: false,
+      confirm: null,
+      message: null,
+      showSnackbar:false
+    }
+  },
   props: ['event','index'],
   methods: mapActions({deleteEvent:'deleteEvent'}),
   methods: {
@@ -62,6 +83,17 @@ export default {
       array.push(index)
       array.push(id)
       store.dispatch('deleteEvent',{index: array[0],id: array[1]})
+    },
+    onCancel() {
+      this.confirm = 'Disagreed'
+      this.message = 'Share cancelled'
+      this.showSnackbar = true
+
+    },
+    onConfirm() {
+      this.confirm = 'Agreed'
+      this.message = 'Sharing Event...'
+      this.showSnackbar = true
     }
   }
 }
