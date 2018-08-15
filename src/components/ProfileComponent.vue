869b195119e47c4fee487678aa22e6570d1f71e6
Gabriel Rizzo <grizzo01@hotmail.com>
2018-08-15 10:15:34 -0300

    Fix no component de profile

--- src/components/ProfileComponent.vue
+++ src/components/ProfileComponent.vue
@@ -1,11 +1,17 @@
 <template>
-    <h3>{{userEmail}}</h3>
+    <h3>{{email}}</h3>
 </template>
 
 <script>
   import {mapGetters} from 'vuex'
   export default {
-    computed: mapGetters({userEmail:'getUserEmail'})
+    //computed: mapGetters({userEmail:'getUserEmail'}),
+
+    data() {
+      return {
+        email:this.$route.params.user
+      }
+    }
 
   }
 </script>
